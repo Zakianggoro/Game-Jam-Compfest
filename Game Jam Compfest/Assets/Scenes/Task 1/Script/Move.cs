10d40b0aa0e62bcc176f02cc565976382fe63d2a
Phoenixops <mzakianggoro324@gmail.com>
2024-08-04 16:28:45 +0700

    .

--- Game Jam Compfest/Assets/Scenes/Task 1/Script/Move.cs
+++ Game Jam Compfest/Assets/Scenes/Task 1/Script/Move.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class MovementZ : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
